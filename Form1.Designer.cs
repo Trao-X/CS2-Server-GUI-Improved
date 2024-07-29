1939dde2463a223f0850d88f8078d7437a90afab
TraoX_ <140246452+Trao-X@users.noreply.github.com>
2024-07-29 23:14:20 +0300

    Version 1.1.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,26 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.start_btn = new System.Windows.Forms.Button();
             this.title = new System.Windows.Forms.Label();
             this.game_mode = new System.Windows.Forms.Label();
             this.game_mode_sel = new System.Windows.Forms.ComboBox();
             this.game_type = new System.Windows.Forms.Label();
             this.game_type_sel = new System.Windows.Forms.ComboBox();
-            this.map_sel = new System.Windows.Forms.ComboBox();
             this.map = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // start_btn
             // 
-            this.start_btn.Location = new System.Drawing.Point(116, 277);
+            this.start_btn.Location = new System.Drawing.Point(12, 459);
             this.start_btn.Name = "start_btn";
             this.start_btn.Size = new System.Drawing.Size(141, 44);
             this.start_btn.TabIndex = 0;
@@ -58,175 +61,171 @@
             // title
             // 
             this.title.AutoSize = true;
-            this.title.Font = new System.Drawing.Font("HP Simplified", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title.Location = new System.Drawing.Point(22, 9);
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(12, 29);
             this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(313, 24);
+            this.title.Size = new System.Drawing.Size(225, 33);
             this.title.TabIndex = 1;
-            this.title.Text = "Easy Counter-Strike 2 Server Config";
-            this.title.Click += new System.EventHandler(this.title_Click);
+            this.title.Text = "CS2 Server GUI";
             // 
             // game_mode
             // 
             this.game_mode.AutoSize = true;
-            this.game_mode.Font = new System.Drawing.Font("HP Simplified", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.game_mode.Location = new System.Drawing.Point(73, 45);
+            this.game_mode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.game_mode.Location = new System.Drawing.Point(14, 268);
             this.game_mode.Name = "game_mode";
-            this.game_mode.Size = new System.Drawing.Size(83, 18);
+            this.game_mode.Size = new System.Drawing.Size(97, 20);
             this.game_mode.TabIndex = 2;
-            this.game_mode.Text = "Game Mode:";
-            this.game_mode.Click += new System.EventHandler(this.game_mode_Click);
+            this.game_mode.Text = "Game Mode";
             // 
             // game_mode_sel
             // 
             this.game_mode_sel.FormattingEnabled = true;
             this.game_mode_sel.Items.AddRange(new object[] {
-            "0",
-            "1",
-            "2",
-            "3",
-            "4"});
-            this.game_mode_sel.Location = new System.Drawing.Point(162, 45);
+            "0 (casual)",
+            "1 (competitive)",
+            "2 (wingman)",
+            "3 (weapons expert)",
+            "4 (training day)"});
+            this.game_mode_sel.Location = new System.Drawing.Point(122, 266);
             this.game_mode_sel.Name = "game_mode_sel";
-            this.game_mode_sel.Size = new System.Drawing.Size(141, 21);
+            this.game_mode_sel.Size = new System.Drawing.Size(121, 21);
             this.game_mode_sel.TabIndex = 3;
             this.game_mode_sel.SelectedIndexChanged += new System.EventHandler(this.game_mode_SelectedIndexChanged);
             // 
             // game_type
             // 
             this.game_type.AutoSize = true;
-            this.game_type.Font = new System.Drawing.Font("HP Simplified", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.game_type.Location = new System.Drawing.Point(73, 74);
+            this.game_type.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.game_type.Location = new System.Drawing.Point(14, 297);
             this.game_type.Name = "game_type";
-            this.game_type.Size = new System.Drawing.Size(80, 18);
+            this.game_type.Size = new System.Drawing.Size(91, 20);
             this.game_type.TabIndex = 4;
-            this.game_type.Text = "Game Type:";
+            this.game_type.Text = "Game Type";
             // 
             // game_type_sel
             // 
             this.game_type_sel.FormattingEnabled = true;
             this.game_type_sel.Items.AddRange(new object[] {
-            "0",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6"});
-            this.game_type_sel.Location = new System.Drawing.Point(162, 75);
+            "0 (classic)",
+            "1 (gun game)",
+            "2 (training)",
+            "3 (custom)",
+            "4 (coop)",
+            "5 (skirmish)",
+            "6 (ffa)"});
+            this.game_type_sel.Location = new System.Drawing.Point(122, 299);
             this.game_type_sel.Name = "game_type_sel";
-            this.game_type_sel.Size = new System.Drawing.Size(141, 21);
+            this.game_type_sel.Size = new System.Drawing.Size(121, 21);
             this.game_type_sel.TabIndex = 5;
             this.game_type_sel.SelectedIndexChanged += new System.EventHandler(this.game_type_SelectedIndexChanged);
-            // 
-            // map_sel
-            // 
-            this.map_sel.FormattingEnabled = true;
-            this.map_sel.Items.AddRange(new object[] {
-            "cs_italy",
-            "cs_office",
-            "de_ancient",
-            "de_anubis",
-            "de_dust2",
-            "de_inferno",
-            "de_mirage",
-            "de_nuke",
-            "de_overpass",
-            "de_vertigo"});
-            this.map_sel.Location = new System.Drawing.Point(162, 105);
-            this.map_sel.Name = "map_sel";
-            this.map_sel.Size = new System.Drawing.Size(141, 21);
-            this.map_sel.TabIndex = 7;
-            this.map_sel.SelectedIndexChanged += new System.EventHandler(this.map_sel_SelectedIndexChanged);
             // 
             // map
             // 
             this.map.AutoSize = true;
-            this.map.Font = new System.Drawing.Font("HP Simplified", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.map.Location = new System.Drawing.Point(112, 103);
+            this.map.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.map.Location = new System.Drawing.Point(14, 328);
             this.map.Name = "map";
-            this.map.Size = new System.Drawing.Size(38, 18);
+            this.map.Size = new System.Drawing.Size(40, 20);
             this.map.TabIndex = 6;
-            this.map.Text = "Map:";
+            this.map.Text = "Map";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 327);
+            this.button1.Location = new System.Drawing.Point(199, 480);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(339, 23);
+            this.button1.Size = new System.Drawing.Size(169, 23);
             this.button1.TabIndex = 8;
-            this.button1.Text = "Information about the game modes and game types.";
+            this.button1.Text = "Information on modes and types.";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // button2
             // 
-            this.textBox1.Location = new System.Drawing.Point(11, 146);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(340, 20);
-            this.textBox1.TabIndex = 9;
+            this.button2.Location = new System.Drawing.Point(253, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Select Game Path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("HP Simplified", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 129);
+            this.label1.Location = new System.Drawing.Point(197, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(180, 14);
+            this.label1.Size = new System.Drawing.Size(174, 13);
             this.label1.TabIndex = 10;
-            this.label1.Text = "The Counter-Strike 2 executable file:";
+            this.label1.Text = "Select your CS2 executable above.";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 169);
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(12, 121);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(326, 13);
+            this.label2.Size = new System.Drawing.Size(358, 29);
             this.label2.TabIndex = 11;
-            this.label2.Text = "In order to find the executable, go into where you installed the game";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 182);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(332, 13);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "then after that go to /game/bin/win64 and hold shift then right click it";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 195);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+            this.label4.Location = new System.Drawing.Point(12, 99);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(289, 13);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "and then you want to copy it as path. Paste it in the textbox.";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Executable Path";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(122, 328);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Select Map";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(12, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(358, 29);
+            this.label3.TabIndex = 17;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(8, 208);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+            this.label5.Location = new System.Drawing.Point(12, 161);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(333, 15);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "And don\'t forget to remove the \"s at the start and end!";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Map Path";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 362);
+            this.ClientSize = new System.Drawing.Size(383, 516);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.map_sel);
             this.Controls.Add(this.map);
             this.Controls.Add(this.game_type_sel);
             this.Controls.Add(this.game_type);
@@ -234,9 +233,12 @@
             this.Controls.Add(this.game_mode);
             this.Controls.Add(this.title);
             this.Controls.Add(this.start_btn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(399, 554);
+            this.MinimumSize = new System.Drawing.Size(399, 554);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "CS2 Server GUI";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -250,15 +252,17 @@
         private System.Windows.Forms.ComboBox game_mode_sel;
         private System.Windows.Forms.Label game_type;
         private System.Windows.Forms.ComboBox game_type_sel;
-        private System.Windows.Forms.ComboBox map_sel;
         private System.Windows.Forms.Label map;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace cs2servergui
@@ -18,96 +9,106 @@ namespace cs2servergui
         public Form1()
         {
             InitializeComponent();
+            Global.Initialize();
+            label2.Text = Global.CS2Path;
         }
+
         public static class Global
         {
-            public static string game_mode;
-            public static string game_type;
-            public static string map;
-            public static string cs2_path;
-            public static string cs2_srv_args;
+            public static int GameMode { get; set; }
+            public static int GameType { get; set; }
+            public static string CS2Path { get; set; }
+            public static string CS2MapFile { get; set; }
+            public static string CS2SrvArgs { get; set; }
+
+            public static void Initialize()
+            {
+                CS2MapFile = null;
+                CS2Path = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Counter-Strike Global Offensive\\game\\bin\\win64\\cs2.exe";
+            }
         }
+
         private void start_btn_Click(object sender, EventArgs e)
         {
-            // Get the text from textBox1 and store it in the 'path' variable
-            string path = textBox1.Text;
-
-            // Check if the path is not empty and is an absolute path
-            if (!string.IsNullOrEmpty(path) && Path.IsPathRooted(path))
+            try
             {
-                // Set the Global.cs2_path variable to the 'path' value
-                Global.cs2_path = path;
-
-                // Clear any previous error message
-                label1.Text = "The Counter-Strike 2 executable file:";
-
-                // Set the Global.cs2_srv_args
-                Global.cs2_srv_args = " -dedicated -insecure " + "+game_mode " + Global.game_mode + " +game_type " + Global.game_type + " +map " + Global.map;
-
-                Process p = new Process();
-                p.StartInfo.FileName = Global.cs2_path;
-                p.StartInfo.Arguments = Global.cs2_srv_args;
-                p.Start();
+                if (string.IsNullOrEmpty(Global.CS2Path))
+                {
+                    MessageBox.Show("Please select a path for cs2.exe.", "Error: Cannot find cs2.exe", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    Global.CS2SrvArgs = $"-dedicated -insecure +game_mode {Global.GameMode} +game_type {Global.GameType} +map {Global.CS2MapFile}";
+                    Process p = new Process
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = Global.CS2Path,
+                            Arguments = Global.CS2SrvArgs
+                        }
+                    };
+                    p.Start();
+                }
             }
-            else
+            catch (Exception ex)
             {
-                // Display "Invalid Path!" in label1
-                label1.Text = "Invalid Path!";
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
         }
+
         private void game_mode_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Global.game_mode = game_mode_sel.SelectedIndex.ToString();
+            if (int.TryParse(game_mode_sel.SelectedIndex.ToString(), out int mode))
+            {
+                Global.GameMode = mode;
+            }
         }
 
         private void game_type_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Global.game_type = game_type_sel.SelectedIndex.ToString();
-        }
-
-        private void map_sel_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            Global.map = map_sel.SelectedItem.ToString();
+            if (int.TryParse(game_type_sel.SelectedIndex.ToString(), out int type))
+            {
+                Global.GameType = type;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start(new ProcessStartInfo
+            Process.Start(new ProcessStartInfo
             {
                 FileName = "https://developer.valvesoftware.com/wiki/Counter-Strike:_Global_Offensive/Game_Modes",
                 UseShellExecute = true
             });
         }
 
-        private void title_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
+            openFileDialog1.InitialDirectory = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Counter-Strike Global Offensive";
+            openFileDialog1.Filter = "cs2.exe|cs2.exe|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.FileName = string.Empty; // Ensure no file is pre-selected
 
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Get the path of cs2.exe
+                Global.CS2Path = openFileDialog1.FileName;
+                label2.Text = Global.CS2Path;
+            }
         }
 
-        private void game_mode_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
+            openFileDialog2.InitialDirectory = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Counter-Strike Global Offensive\\game\\csgo\\maps";
+            openFileDialog2.Filter = "*.vpk|*.vpk|All files (*.*)|*.*";
+            openFileDialog2.FilterIndex = 1;
+            openFileDialog2.FileName = string.Empty; // Ensure no file is pre-selected
 
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void folderBrowserDialog2_HelpRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                // Get the path of the map file
+                Global.CS2MapFile = $"\"{openFileDialog2.FileName}\"";
+                label3.Text = Global.CS2MapFile;
+            }
         }
     }
 }
